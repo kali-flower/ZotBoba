a54de1ce0ab1009f98ff4538560d70c6639a1aa6
kali-flower <sdaschou@uci.edu>
2026-03-11 19:11:05 -0700

    feat: added page so users can edit favorites/rankings

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -5,75 +5,80 @@ import { router } from "expo-router";
 import { useEffect, useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
-
 export default function NavBar() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        const checkUser = async () => {
-            const user = await getCurrentUser();
-            setIsLoggedIn(!!user);
-        }
-        checkUser();
-    }, []);
+	useEffect(() => {
+		const checkUser = async () => {
+			const user = await getCurrentUser();
+			setIsLoggedIn(!!user);
+		};
+		checkUser();
+	}, []);
 
-    return (
-        <View style={styles.container}>
+	return (
+		<View style={styles.container}>
+			<View style={styles.home}>
+				<Pressable onPress={() => router.push("/(tabs)")}>
+					<Text style={styles.title}>ZotBoba</Text>
+				</Pressable>
+			</View>
 
-            <View style={styles.home}>
-                <Pressable onPress={() => router.push('/(tabs)')}>
-                    <Text style={styles.title}>ZotBoba</Text>
-                </Pressable>
-            </View>
+			<View style={styles.rightButtons}>
+				<Pressable onPress={() => router.push("/(tabs)")}>
+					<Text style={styles.subtitle}>Home</Text>
+				</Pressable>
 
-            <View style={styles.rightButtons}>
-                <Pressable onPress={() => router.push('/(tabs)')}>
-                    <Text style={styles.subtitle}>Home</Text>
-                </Pressable>
+				<Pressable
+					onPress={() =>
+						router.push(isLoggedIn ? "/(tabs)/search" : "/(tabs)/login")
+					}
+				>
+					<Text style={styles.subtitle}>Search</Text>
+				</Pressable>
 
-                <Pressable onPress={() => router.push(isLoggedIn ? '/(tabs)/search' : '/(tabs)/login')}>
-                    <Text style={styles.subtitle}>Search</Text>
-                </Pressable>
-
-                <Pressable onPress={() => router.push(isLoggedIn ? '/(tabs)/settings' : '/(tabs)/login')}>
-                    <Text style={styles.subtitle}>{
-                        isLoggedIn ? "Account" : "Login"
-                    }</Text>
-                </Pressable>
-            </View>
-
-        </View>
-    );
+				<Pressable
+					onPress={() =>
+						router.push(isLoggedIn ? "/(tabs)/account" : "/(tabs)/login")
+					}
+				>
+					<Text style={styles.subtitle}>
+						{isLoggedIn ? "Account" : "Login"}
+					</Text>
+				</Pressable>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: 80,
-        backgroundColor: colors.navbar,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingRight: 30,
-        paddingLeft: 30,
-        zIndex: 1,
-        borderBottomColor: 'black',
-        borderBottomWidth: 1
-    },
-    home: {
-        justifyContent: "flex-start",
-    },
-    rightButtons: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 16
-    },
-    title: {
-        ...typography.title,
-    },
-    body: {
-        ...typography.body,
-    },
-    subtitle: {
-        ...typography.subtitle,
-    },
+	container: {
+		height: 80,
+		backgroundColor: colors.navbar,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		paddingRight: 30,
+		paddingLeft: 30,
+		zIndex: 1,
+		borderBottomColor: "black",
+		borderBottomWidth: 1,
+	},
+	home: {
+		justifyContent: "flex-start",
+	},
+	rightButtons: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 16,
+	},
+	title: {
+		...typography.title,
+	},
+	body: {
+		...typography.body,
+	},
+	subtitle: {
+		...typography.subtitle,
+	},
 });
